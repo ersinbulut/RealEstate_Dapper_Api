da337ee9caa114f10c8627c63b8292bfdb09ef34
DEV <DEV@DESKTOP-8VP4IIT>
2024-04-29 17:17:52 +0300

    #7 Real Estate - AspNet Core 8.0 + Api + Dapper - Kategorilerin Api ile Silinmesi ve Güncellenmesi

---
+++ RealEstate_Dapper_Api/Models/DapperContext/Context.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace RealEstate_Dapper_Api.Models.DapperContext
+{
+    public class Context
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionstring;
+
+        public Context(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionstring = _configuration.GetConnectionString("connection");
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionstring);
+
+
+    }
+}
