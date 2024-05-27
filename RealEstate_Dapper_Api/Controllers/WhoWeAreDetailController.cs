6dbf894d46e44563000be6c6695f614f61c90edf
DEV <DEV@DESKTOP-8VP4IIT>
2024-05-27 17:05:05 +0300

    #14 Real Estate - AspNet Core 8.0 + Api + Dapper - WhoWeAreDetail Api

---
+++ RealEstate_Dapper_Api/Controllers/WhoWeAreDetailController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Dtos.WhoWeAreDetailDtos;
+using RealEstate_Dapper_Api.Repositories.WhoWeAreRepository;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WhoWeAreDetailController : ControllerBase
+    {
+        private readonly IWhoWeAreDetailRepository _WhoWeAreDetailRepository;
+
+        public WhoWeAreDetailController(IWhoWeAreDetailRepository WhoWeAreDetailRepository)
+        {
+            _WhoWeAreDetailRepository = WhoWeAreDetailRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> WhoWeAreDetailList()
+        {
+            var values = await _WhoWeAreDetailRepository.GetAllWhoWeAreDetailAsync();
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateWhoWeAreDetail(CreateWhoWeAreDetailDto createWhoWeAreDetailDto)
+        {
+            _WhoWeAreDetailRepository.CreateWhoWeAreDetail(createWhoWeAreDetailDto);
+            return Ok("Hakkımızda Başarılı Bir Şekilde Eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteWhoWeAreDetail(int id)
+        {
+            _WhoWeAreDetailRepository.DeleteWhoWeAreDetail(id);
+            return Ok("Hakkımızda Başarılı Bir Şekilde Silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateWhoWeAreDetail(UpdateWhoWeAreDetailDto updateWhoWeAreDetailDto)
+        {
+            _WhoWeAreDetailRepository.UpdateWhoWeAreDetail(updateWhoWeAreDetailDto);
+            return Ok("Hakkımızda Başarılı Bir Şekilde Güncellendi");
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetWhoWeAreDetail(int id)
+        {
+            var value = await _WhoWeAreDetailRepository.GetWhoWeAreDetail(id);
+            return Ok(value);
+        }
+    }
+}
