af0161a7203f2a023c330591ff68dcdbd4d59cb9
DEV <DEV@DESKTOP-8VP4IIT>
2024-05-29 13:58:25 +0300

    #20 Real Estate - AspNet Core 8.0 + Api + Dapper - Popüler Lokasyonlar

---
+++ RealEstate_Dapper_Api/Repositories/PopularLocationRepositories/IPopularLocationRepository.cs
@@ -0,0 +1,13 @@
+﻿using RealEstate_Dapper_Api.Dtos.PopularLocationDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.PopularLocationRepositories
+{
+    public interface IPopularLocationRepository
+    {
+        Task<List<ResultPopularLocationDto>> GetAllPopulerLocationAsync();
+        //void CreatePopulerLocation(CreatePopulerLocationDto PopulerLocationDto);
+        //void DeletePopulerLocation(int id);
+        //void UpdatePopulerLocation(UpdatePopulerLocationDto PopulerLocationDto);
+        //Task<GetPopulerLocationDto> GetPopulerLocation(int id);
+    }
+}
--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultProductListExploreCitiesComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultProductListExploreCitiesComponentPartial.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.PopulerLocationDtos;
 
 namespace RealEstate_Dapper_UI.ViewComponents.HomePage
 {
     public class _DefaultProductListExploreCitiesComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultProductListExploreCitiesComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsiveMessage = await client.GetAsync("https://localhost:7285/api/PopularLocations/");
+            if (responsiveMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsiveMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultPopulerLocationDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
