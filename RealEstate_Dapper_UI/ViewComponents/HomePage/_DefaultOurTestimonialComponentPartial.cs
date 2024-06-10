332e1c78deb2cd04a1325db650e4cad36b03b35f
DEV <DEV@DESKTOP-8VP4IIT>
2024-06-10 16:34:59 +0300

    #22 Real Estate - AspNet Core 8.0 + Api + Dapper - Eksik Tabloların Tamamlanması

---
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultOurTestimonialComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.TestimonialDtos;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+{
+    public class _DefaultOurTestimonialComponentPartial: ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultOurTestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsiveMessage = await client.GetAsync("https://localhost:7285/api/Testimonials/");
+            if (responsiveMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsiveMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
