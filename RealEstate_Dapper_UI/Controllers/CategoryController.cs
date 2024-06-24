a968c021cbfad1dccb5592b77a41979d73ac222e
DEV <DEV@DESKTOP-8VP4IIT>
2024-06-24 14:01:59 +0300

    #24 Real Estate - AspNet Core 8.0 + Api + Dapper - Admin Panelinde Kategori Apinin Consume Edilmesi

---
+++ RealEstate_Dapper_UI/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.CategoryDtos;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsiveMessage = await client.GetAsync("https://localhost:7285/api/Categories/");
+            if (responsiveMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsiveMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+    }
+}
