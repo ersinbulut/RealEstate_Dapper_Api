a185fec4d18f5b64f2d8907c1b88971172b013d4
DEV <DEV@DESKTOP-8VP4IIT>
2024-12-17 08:00:00 +0300

    #30 Real Estate - AspNet Core 8.0 + Api + Dapper - Employee İşlemleri

---
+++ RealEstate_Dapper_UI/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.EmployeeDtos;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public EmployeeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsiveMessage = await client.GetAsync("https://localhost:7285/api/Employees/");
+            if (responsiveMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsiveMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultEmployeeDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
