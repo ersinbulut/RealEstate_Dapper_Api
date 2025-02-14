10cf8b308c44ef5ab88fccf67c49f6dbed1254e3
Developer <Developer@DEV>
2025-02-14 08:50:59 +0300

    #64 Real Estate - AspNet Core 8.0 + Api + Dapper - Giriş Yapan Emlakçıya Ait Dashboard İstatistikler

---
+++ RealEstate_Dapper_Api/Controllers/EstateAgentDashboardStatisticController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.EstateAgentRepositories.DashboardRepositories.StatisticRepositories;
+
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstateAgentDashboardStatisticController : ControllerBase
+    {
+        private readonly IStatisticRepository _statisticsRepository;
+
+        public EstateAgentDashboardStatisticController(IStatisticRepository statisticsRepository)
+        {
+            _statisticsRepository = statisticsRepository;
+        }
+        //aktif kategori sayısı
+        [HttpGet("AllProductCount")]
+        public IActionResult AllProductCount()
+        {
+            return Ok(_statisticsRepository.AllProductCount());
+        }
+
+        [HttpGet("ProductCountByEmployeeId")]
+        public IActionResult ProductCountByEmployeeId(int id)
+        {
+            return Ok(_statisticsRepository.ProductCountByEmployeeId(id));
+        }
+
+        [HttpGet("ProductCountByStatusFalse")]
+        public IActionResult ProductCountByStatusFalse(int id)
+        {
+            return Ok(_statisticsRepository.ProductCountByStatusFalse(id));
+        }
+
+        [HttpGet("ProductCountByStatusTrue")]
+        public IActionResult ProductCountByStatusTrue(int id)
+        {
+            return Ok(_statisticsRepository.ProductCountByStatusTrue(id));
+        }
+
+    }
+}
---
+++ RealEstate_Dapper_UI/ViewComponents/EstateAgent/_EstateAgentDashboardStatisticsComponentPartial.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponents.EstateAgent
+{
+    public class _EstateAgentDashboardStatisticsComponentPartial:ViewComponent
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _EstateAgentDashboardStatisticsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            #region İstatistik1 - Toplam İlan Sayısı
+            var client1 = _httpClientFactory.CreateClient();
+            var responseMessage1 = await client1.GetAsync("https://localhost:7285/api/EstateAgentDashboardStatistic/AllProductCount");
+            var jsonData1 = await responseMessage1.Content.ReadAsStringAsync();
+            ViewBag.ProductCount = jsonData1;
+            #endregion
+            #region İstatistik2 - Emlakçının Toplam İlan Sayısı
+            var client2 = _httpClientFactory.CreateClient();
+            var responseMessage2 = await client2.GetAsync("https://localhost:7285/api/EstateAgentDashboardStatistic/ProductCountByEmployeeId?id=1");
+            var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+            ViewBag.EmployeeByProductCount = jsonData2;
+            #endregion
+            #region İstatistik3 - Aktif İlan Sayısı
+            var client3 = _httpClientFactory.CreateClient();
+            var responseMessage3 = await client3.GetAsync("https://localhost:7285/api/EstateAgentDashboardStatistic/ProductCountByStatusTrue?id=1");
+            var jsonData3 = await responseMessage3.Content.ReadAsStringAsync();
+            ViewBag.productCountByEmployeeByStatusTrue = jsonData3;
+            #endregion
+            #region İstatistik4 - Pasif İlan Sayısı
+            var client4 = _httpClientFactory.CreateClient();
+            var responseMessage4 = await client4.GetAsync("https://localhost:7285/api/EstateAgentDashboardStatistic/ProductCountByStatusFalse?id=1");
+            var jsonData4 = await responseMessage4.Content.ReadAsStringAsync();
+            ViewBag.productCountByEmployeeByStatusFalse = jsonData4;
+            #endregion
+
+
+            return View();
+        }
+    }
+}
