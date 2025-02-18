dd75c34c78f694aa9c083272e9f54945ec8ae892
Developer <Developer@DEV>
2025-02-18 12:39:48 +0300

    #67 Real Estate - AspNet Core 8.0 + Api + Dapper - Emlakçıya Özgü Mesaj Listesinin Api Listesi

---
+++ RealEstate_Dapper_Api/Repositories/MessageRepositories/IMessageRepository.cs
@@ -0,0 +1,10 @@
+﻿using RealEstate_Dapper_Api.Dtos.MessageDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.MessageRepositories
+{
+    public interface IMessageRepository
+    {
+        Task<List<ResultInboxMessageDto>> GetInboxLast3MessageListByReceiver(int id);
+    }
+}
+ 
--- RealEstate_Dapper_UI/ViewComponents/EstateAgent/_EstateAgentDashboardChartComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/EstateAgent/_EstateAgentDashboardChartComponentPartial.cs
@@ -4,10 +4,9 @@ using RealEstate_Dapper_UI.Dtos.EstateAgentDtos;
 
 namespace RealEstate_Dapper_UI.ViewComponents.EstateAgent
 {
-    public class _EstateAgentDashboardChartComponentPartial:ViewComponent
+    public class _EstateAgentDashboardChartComponentPartial : ViewComponent
     {
         private readonly IHttpClientFactory _httpClientFactory;
-
         public _EstateAgentDashboardChartComponentPartial(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
@@ -15,10 +14,10 @@ namespace RealEstate_Dapper_UI.ViewComponents.EstateAgent
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
-            var responsiveMessage = await client.GetAsync("https://localhost:7285/api/EstateAgentChart/");
-            if (responsiveMessage.IsSuccessStatusCode)
+            var responseMessage = await client.GetAsync("https://localhost:7285/api/EstateAgentChart");
+            if (responseMessage.IsSuccessStatusCode)
             {
-                var jsonData = await responsiveMessage.Content.ReadAsStringAsync();
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultEstateAgentDashboardChartDto>>(jsonData);
                 return View(values);
             }
