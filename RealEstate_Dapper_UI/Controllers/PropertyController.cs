849bd5ba18fdcd3653725d3ff98c71e0045317ac
Developer <Developer@DEV>
2025-02-19 08:27:32 +0300

    #72 Real Estate - AspNet Core 8.0 + Api + Dapper - Single Property Sayfası

---
+++ RealEstate_Dapper_UI/Controllers/PropertyController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.ProductDtos;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    public class PropertyController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PropertyController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsiveMessage = await client.GetAsync("https://localhost:7285/api/Products/ProductListWithCategory");
+            if (responsiveMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsiveMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDtos>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PropertySingle(int id)
+        {
+            return View();
+        }
+    }
+}
