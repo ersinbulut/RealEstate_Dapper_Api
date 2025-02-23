5dddf2b9fd0f677607ec5f09aa43a9b2246da2e8
BULUTS <BULUTS@BULUTS>
2025-02-23 19:02:21 +0300

    #85 Real Estate - AspNet Core 8.0 + Api + Dapper - Ev Arama Sayfası İçin Partial Oluşturma

--- RealEstate_Dapper_UI/Controllers/DefaultController.cs
+++ RealEstate_Dapper_UI/Controllers/DefaultController.cs
@@ -1,24 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.CategoryDtos;
 
 namespace RealEstate_Dapper_UI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpGet]
-        public PartialViewResult PartialSearch()
+        public async Task<PartialViewResult> PartialSearch()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responsiveMessage = await client.GetAsync("https://localhost:7285/api/Categories/");
+            if (responsiveMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsiveMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return PartialView(values);
+            }
             return PartialView();
         }
 
         [HttpPost]
-        public IActionResult PartialSearch(string p)
+        public IActionResult PartialSearch(string p,string y)
         {
             TempData["word"] = p;
+            TempData["word1"] = y;
             return RedirectToAction("PropertyListWithSearch","Property");
         }
     }
--- RealEstate_Dapper_UI/Controllers/PropertyController.cs
+++ RealEstate_Dapper_UI/Controllers/PropertyController.cs
@@ -30,6 +30,7 @@ namespace RealEstate_Dapper_UI.Controllers
         public async Task<IActionResult> PropertyListWithSearch(string searchKeyValue, int propertyCategoryId, string city)
         {
             ViewBag.v = TempData["word"];
+            ViewBag.y = TempData["word1"];
             searchKeyValue = "daire";
             propertyCategoryId = 1;
             city = "izmir";
