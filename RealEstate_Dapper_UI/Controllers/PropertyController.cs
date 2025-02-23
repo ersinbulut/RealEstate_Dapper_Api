d0771a5606fa2dd7a5488fc27cfe46bfdb7061f4
BULUTS <BULUTS@BULUTS>
2025-02-23 17:45:52 +0300

    #81 Real Estate - AspNet Core 8.0 + Api + Dapper - Amenity Consume ve Map Video Embed İşlemleri

--- RealEstate_Dapper_UI/Controllers/PropertyController.cs
+++ RealEstate_Dapper_UI/Controllers/PropertyController.cs
@@ -57,8 +57,10 @@ namespace RealEstate_Dapper_UI.Controllers
             ViewBag.garageCount = values2.garageSize;
             ViewBag.buildYear = values2.buildYear;
             ViewBag.date = values.AdvertisementDate;
+            ViewBag.location = values2.location;
+            ViewBag.videoUrl = values2.videoUrl;
 
-            DateTime date1=DateTime.Now;
+           DateTime date1=DateTime.Now;
             DateTime date2 = values.AdvertisementDate;
 
             TimeSpan timeSpan=date1-date2;
---
+++ RealEstate_Dapper_UI/ViewComponents/PropertySingle/_PropertyAmenityStatusTrueByPropertyIdComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.PropertyAmenityDtos;
+
+namespace RealEstate_Dapper_UI.ViewComponents.PropertySingle
+{
+    public class _PropertyAmenityStatusTrueByPropertyIdComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _PropertyAmenityStatusTrueByPropertyIdComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsiveMessage = await client.GetAsync("https://localhost:7285/api/PropertyAmenities?id=1");
+            if (responsiveMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsiveMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultPropertyAmenityByStatusTrueDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+    }
+}
