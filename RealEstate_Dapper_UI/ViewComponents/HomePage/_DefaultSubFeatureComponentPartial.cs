4418ec0561fefbe40815f58c8e6480eb96db038e
Developer <Developer@DEV>
2025-02-24 10:02:04 +0300

    #90 Real Estate - AspNet Core 8.0 + Api + Dapper - Ana Sayfada Son Revizeler 2

--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultSubFeatureComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultSubFeatureComponentPartial.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.SubFeatureDtos;
 
 namespace RealEstate_Dapper_UI.ViewComponents.HomePage
 {
     public class _DefaultSubFeatureComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultSubFeatureComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsiveMessage = await client.GetAsync("https://localhost:7285/api/SubFeatures/");
+            if (responsiveMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsiveMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSubFeatureDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
